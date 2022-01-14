413c3c986a633c0469805c1b058d24047d52289f
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-01-14 09:36:56 +0530

    getting data

--- client/src/components/Public.js
+++ client/src/components/Public.js
@@ -1,7 +1,8 @@
 import React,{useState, useEffect} from 'react'
-
+import { useSelector } from 'react-redux'
 const Public = () => {
     const [data, setData] = useState([])
+    const {page} = useSelector(state => state.pageState)
     const getpublic = () => {
         fetch(`https://api.github.com/gists/public?page=${page}&per_page=5`)
         .then((res) => res.json())
@@ -15,9 +16,18 @@ const Public = () => {
     return (
         <div>
             {
-                data.map((e) => {
-                    
-                })
+                data.map((e) => (
+                    <div className='public_cards'>
+                        <div>
+                            <img src={e.owner.avatar_url} alt="" />
+                        </div>
+                        <div>
+                        <p>{e.owner.login}</p>
+                        <a href={e.owner.html_url} target="_blank">Go To GitHub</a>
+                        <a href={e.html_url}>Go To GitHub Gist</a>
+                        </div>
+                    </div>
+                ))
             }
         </div>
     )
